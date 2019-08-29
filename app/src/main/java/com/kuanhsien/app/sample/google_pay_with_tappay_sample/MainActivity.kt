ba6fc4e58b69669670a3d0427c28dddae95df603
KuanHsien <coson002fy@gmail.com>
2019-08-29 16:57:41 +0800

    Initial commit

---
+++ app/src/main/java/com/kuanhsien/app/sample/google_pay_with_tappay_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kuanhsien.app.sample.google_pay_with_tappay_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
